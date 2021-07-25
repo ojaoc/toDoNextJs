8caa335ebe993ff1c5236f34f76381b485e2844b
João Correia <correia.jafonso@gmail.com>
2021-07-25 16:01:32 +0100

    Changed theme to resolved theme to keep track of system ui

--- components/Header.jsx
+++ components/Header.jsx
@@ -7,9 +7,9 @@ import { useTheme } from 'next-themes';
 import DarkModeBtn from './DarkModeBtn';
 
 const Header = () => {
-  const { theme, setTheme } = useTheme();
+  const { resolvedTheme, setTheme } = useTheme();
 
-  const toggleDarkMode = () => (theme === 'dark' ? setTheme('light') : setTheme('dark'));
+  const toggleDarkMode = () => (resolvedTheme === 'dark' ? setTheme('light') : setTheme('dark'));
 
   return (
     <>
@@ -19,7 +19,7 @@ const Header = () => {
           To Do
         </div>
         <div className={darkModeContainer}>
-          <DarkModeBtn darkModeEnabled={theme === 'dark'} iconClass={iconButton} onClick={toggleDarkMode} />
+          <DarkModeBtn darkModeEnabled={resolvedTheme === 'dark'} iconClass={iconButton} onClick={toggleDarkMode} />
         </div>
       </div>
       <hr className={separator} />
