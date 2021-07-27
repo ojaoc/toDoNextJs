a6ca1d9a7bb314183eda13b5066e66bcc884a992
João Correia <correia.jafonso@gmail.com>
2021-07-27 16:02:44 +0100

    Fixed dark mode bug

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../styles/globals.scss';
 import { container } from 'styles/App.module.scss';
 import '@fontsource/josefin-sans';
@@ -6,8 +6,15 @@ import '@fontsource/roboto';
 import { IconContext } from 'react-icons/lib';
 import { ThemeProvider } from 'next-themes';
 import Header from 'components/Header';
+import Spinner from 'components/Spinner';
 
 function MyApp({ Component, pageProps }) {
+  const [mounted, setMounted] = useState(false);
+  // When mounted on client, now we can show the UI
+  useEffect(() => setMounted(true), []);
+
+  if (!mounted) return <Spinner active />;
+
   return (
     <ThemeProvider>
       <IconContext.Provider value={{ style: { verticalAlign: 'middle' } }}>
