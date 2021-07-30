6d7c8bc89ee6cff3ca9a6a7014f8170bf26a727a
João Correia <correia.jafonso@gmail.com>
2021-07-30 02:40:29 +0100

    Added clear done tasks functionality

--- components/Board.jsx
+++ components/Board.jsx
@@ -1,17 +1,33 @@
 /* eslint-disable camelcase */
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import { board } from 'styles/Board.module.scss';
+import axios from 'axios';
+import apiBaseUrl from 'utils/apiBaseUrl';
 import BoardItem from './BoardItem';
 import AddBoardItem from './AddBoardItem';
 import ClearDoneTasks from './ClearDoneTasks';
 
 const Board = ({ tasks, mutate }) => {
   const [isAdding, setIsAdding] = useState(false);
+  const [loadingDelete, setLoadingDelete] = useState(false);
+
+  const deleteDoneTasks = useCallback(async () => {
+    setLoadingDelete(true);
+    const listIds = tasks.filter((task) => task.is_done).map((task) => task.id);
+    try {
+      await axios.post(`${apiBaseUrl}/tasks/delete_several`, { task_ids: listIds });
+      mutate();
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setLoadingDelete(false);
+    }
+  }, [tasks]);
 
   return (
     <div className={board}>
       <AddBoardItem isAdding={isAdding} setIsAdding={setIsAdding} mutate={mutate} />
-      <ClearDoneTasks />
+      <ClearDoneTasks onClick={deleteDoneTasks} />
       {tasks?.map(({ description, url, is_done }) => (
         <BoardItem
           key={url}
--- components/ClearDoneTasks.jsx
+++ components/ClearDoneTasks.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const ClearDoneTasks = () => (
-  <button type="button">
+const ClearDoneTasks = ({ onClick }) => (
+  <button type="button" onClick={onClick}>
     Clear
   </button>
 );
